25ff625e36822b29db5c2d2fa983a5dea96a6c58
SiamKing <thesiameseelectric@gmail.com>
2016-12-29 20:39:38 -0800

    built out add a task feature

--- js/app/list/list.controller.js
+++ js/app/list/list.controller.js
@@ -1,11 +1,36 @@
 function ListController () {
   var vm = this;
+  var selectedId = -1;
+  var addFlag = false;
+  var editFlag = false;
+  var removeFlag = false;
+
   vm.currentTask = {};
   vm.add = add;
+  vm.startAdd = startAdd;
+  vm.isInAddMode = isInAddMode;
 
   function add () {
     vm.currentTask.complete = false;
-    vm.list.tasks.push(vm.currentTask)
+    vm.list.tasks.push(vm.currentTask);
+    reset();
+  }
+
+  function startAdd() {
+    reset();
+    addFlag = true;
+    vm.currentTask = {};
+  }
+
+  function isInAddMode() {
+    return addFlag;
+  }
+
+  function reset() {
+    selectedId = -1;
+    addFlag = false;
+    editFlag = false;
+    removeFlag = false;
   }
 
   vm.list = {
